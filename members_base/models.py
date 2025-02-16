be0ef3df329aaff692d94d26499ac09dba31426b
Daniel Jones <tortxof@gmail.com>
2025-02-16 13:01:29 -0500

    Add unique constraints

--- members_base/models.py
+++ members_base/models.py
@@ -16,14 +16,14 @@ class UUIDModel(models.Model):
 
 
 class Office(UUIDModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return f"{self.name}"
 
 
 class MembershipClass(UUIDModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         verbose_name_plural = "Membership classes"
