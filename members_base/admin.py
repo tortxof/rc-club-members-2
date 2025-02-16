66aa86a08c709ad1bfe467831abc2cc8dedb04c4
Daniel Jones <tortxof@gmail.com>
2025-02-16 00:32:47 -0500

    Add admin

--- members_base/admin.py
+++ members_base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from members_base.models import Member, MembershipClass, Office, PhoneNumber
+
+
+class PhoneNumberInline(admin.TabularInline):
+    model = PhoneNumber
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    inlines = [PhoneNumberInline]
+
+
+admin.site.register(MembershipClass)
+admin.site.register(Office)
