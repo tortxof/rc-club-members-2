b4150f2736d36f2a456fbbb4e1ac9cb1f6ec2b62
Daniel Jones <tortxof@gmail.com>
2025-02-16 03:39:01 -0500

    Add views

--- members_base/views.py
+++ members_base/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Member
+
+
+def index(request):
+    return HttpResponse("This is the index.")
+
+
+class MembersListView(ListView):
+    queryset = Member.objects.all().order_by("last_name", "first_name")
+
+
+class MemberDetailView(DetailView):
+    model = Member
