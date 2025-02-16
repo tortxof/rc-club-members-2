55e22092bb4c670b9c263a634b8c861bbf93799d
Daniel Jones <tortxof@gmail.com>
2025-02-16 18:57:18 -0500

    Add APP_ORIGIN setting

--- rc_club_members/settings.py
+++ rc_club_members/settings.py
@@ -131,3 +131,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+APP_ORIGIN = os.getenv("APP_ORIGIN")
