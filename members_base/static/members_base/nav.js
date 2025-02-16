0ce767df8157a33045dd0a9586f8b338ed11a1f9
Daniel Jones <tortxof@gmail.com>
2025-02-16 18:29:32 -0500

    Add templates and static

---
+++ members_base/static/members_base/nav.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const $main_navbar_burger = document.getElementById("main_navbar_burger");
+  const $main_navbar = document.getElementById("main_navbar");
+
+  $main_navbar_burger.addEventListener("click", () => {
+    $main_navbar_burger.classList.toggle("is-active");
+    $main_navbar.classList.toggle("is-active");
+  });
+});
