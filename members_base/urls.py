a24e96da75729ad8f98b3d0b2ffca8687a0c7bea
Daniel Jones <tortxof@gmail.com>
2025-02-16 20:55:28 -0500

    Shorten token login url

--- members_base/urls.py
+++ members_base/urls.py
@@ -5,9 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("sign-out/", views.sign_out, name="sign_out"),
-    path(
-        "ro-token/<str:token>/", views.read_only_token_auth, name="read_only_token_auth"
-    ),
+    path("ro/<str:token>/", views.read_only_token_auth, name="read_only_token_auth"),
     path("email-ro-token/", views.email_read_only_token, name="email_read_only_token"),
     path("members/", views.MembersListView.as_view(), name="members"),
     path(
