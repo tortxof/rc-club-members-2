5e300e16ca9a2980a184a5be082f9d19ae5258a7
Daniel Jones <tortxof@gmail.com>
2025-02-17 12:36:56 -0500

    Fix import

--- members_base/management/commands/import_previous_gen.py
+++ members_base/management/commands/import_previous_gen.py
@@ -1,5 +1,5 @@
+import datetime
 import json
-from datetime import date
 
 from django.core.management.base import BaseCommand
 
@@ -13,19 +13,21 @@ class Command(BaseCommand):
         parser.add_argument("export_file", type=str)
 
     def handle(self, *args, **options):
-        membership_class = MembershipClass.objects.get(name="Adult")
+        membership_class, _ = MembershipClass.objects.get_or_create(name="Adult")
 
         with open(options["export_file"]) as f:
             data = json.load(f)["members"]
 
         for record in data:
             try:
-                expiration_date = date.fromisoformat(record["expire"])
+                expiration_date = datetime.date.fromisoformat(record["expire"])
             except ValueError:
                 expiration_date = None
 
             try:
-                date_of_birth = date.fromisoformat(record["dob"])
+                date_of_birth = datetime.datetime.strptime(
+                    record["dob"], "%a, %d %b %Y %H:%M:%S %Z"
+                ).date()
             except ValueError:
                 date_of_birth = None
 
