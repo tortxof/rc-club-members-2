48e6960cc80f0e3f91005de50b6468ce902dd5ef
Daniel Jones <tortxof@gmail.com>
2025-02-19 20:06:13 -0500

    Add whitenoise STORAGES

--- rc_club_members/settings.py
+++ rc_club_members/settings.py
@@ -150,6 +150,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
